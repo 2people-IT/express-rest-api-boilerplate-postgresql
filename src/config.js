c0c3fd11efb49dabbe38b92cb8107092f4f5e5cd
Sergey Belan <sergeybelan19@gmail.com>
2021-05-04 11:58:41 +0300

    remove mongodb from config, remove mongoose

--- src/config.js
+++ src/config.js
@@ -52,19 +52,10 @@ const config = {
   },
   test: { },
   development: {
-    mongo: {
-      uri: 'mongodb://localhost/express-rest-api-boilerplate-dev',
-      options: {
-        debug: true
-      }
-    }
   },
   production: {
     ip: process.env.IP || undefined,
-    port: process.env.PORT || 8080,
-    mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/express-rest-api-boilerplate'
-    }
+    port: process.env.PORT || 8080
   }
 }
 
